9554596da89d1d889c1568b368e601a63b06030f
WhiteMatt3r <darth.plagueis.sithlord@gmail.com>
2015-10-26 14:20:56 -0400

    Add Project Files

---
+++ Source/pSniff/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace pSniff
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.importToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Protocol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SourceIP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SourcePort = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DestIP = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DestPort = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(1068, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 24);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Scan: On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1143, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exportToolStripMenuItem,
+            this.importToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exportToolStripMenuItem
+            // 
+            this.exportToolStripMenuItem.Name = "exportToolStripMenuItem";
+            this.exportToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.exportToolStripMenuItem.Text = "Export";
+            // 
+            // importToolStripMenuItem
+            // 
+            this.importToolStripMenuItem.Name = "importToolStripMenuItem";
+            this.importToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.importToolStripMenuItem.Text = "Import";
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Protocol,
+            this.SourceIP,
+            this.SourcePort,
+            this.DestIP,
+            this.DestPort});
+            this.listView1.Location = new System.Drawing.Point(0, 25);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(700, 554);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Protocol
+            // 
+            this.Protocol.Text = "Protocol";
+            this.Protocol.Width = 89;
+            // 
+            // SourceIP
+            // 
+            this.SourceIP.Text = "Source IP";
+            this.SourceIP.Width = 121;
+            // 
+            // SourcePort
+            // 
+            this.SourcePort.Text = "Source Port";
+            this.SourcePort.Width = 130;
+            // 
+            // DestIP
+            // 
+            this.DestIP.Text = "Destination IP";
+            this.DestIP.Width = 223;
+            // 
+            // DestPort
+            // 
+            this.DestPort.Text = "Destination Port";
+            this.DestPort.Width = 128;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(987, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 24);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView2
+            // 
+            this.listView2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView2.Location = new System.Drawing.Point(706, 25);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(437, 554);
+            this.listView2.TabIndex = 4;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 582);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(1143, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 587);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Packets Sniffed: 0";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(50, 2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(183, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1143, 604);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(806, 572);
+            this.Name = "Form1";
+            this.Text = "pSniff";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Protocol;
+        private System.Windows.Forms.ColumnHeader SourceIP;
+        private System.Windows.Forms.ColumnHeader SourcePort;
+        private System.Windows.Forms.ColumnHeader DestIP;
+        private System.Windows.Forms.ColumnHeader DestPort;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripMenuItem exportToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem importToolStripMenuItem;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Source/pSniff/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Net;
+using System.Data;
+using System.Linq;
+using System.Net.Sockets;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+
+namespace pSniff
+{
+    public partial class Form1 : Form
+    {
+        private bool running = false;
+        private Stopwatch timer = new Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Start();
+            NetworkInterface[] interfaces = NetworkInterface.GetAllNetworkInterfaces();
+            comboBox1.Items.Add("All");
+            comboBox1.SelectedIndex = 0;
+            foreach (NetworkInterface i in interfaces)
+            {
+                if (i.OperationalStatus == OperationalStatus.Up)
+                {
+                    try
+                    {
+                        String ipv4 = i.GetIPProperties().UnicastAddresses[1].Address.ToString();
+                        if(!ipv4.Contains(":"))
+                        {
+                            comboBox1.Items.Add(new mInterface(ipv4,i));
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            int width = listView1.Width / listView1.Columns.Count;
+            foreach (ColumnHeader header in listView1.Columns)
+            {
+                header.Width = width - 5;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var IPv4Addresses = Dns.GetHostEntry(Dns.GetHostName()).AddressList.Where(al => al.AddressFamily == AddressFamily.InterNetwork).AsEnumerable();
+
+            String bText = button1.Text;
+
+            if (bText == "Scan: On")
+            {
+                running = true;
+                button1.Text = "Scan: Off";
+                if (comboBox1.SelectedIndex == 0)
+                {
+                    foreach (IPAddress ip in IPv4Addresses)
+                        Sniff(ip);
+                }
+                else
+                {
+                    Sniff(IPAddress.Parse(((mInterface)comboBox1.SelectedItem).iAddr));
+                }
+            }
+            else
+            {
+                running = false;
+                button1.Text = "Scan: On";
+            }
+
+        }
+
+        public static string ToProtocolString(byte b)
+        {
+
+            switch (b)
+            {
+                case 1: return "ICMP";
+                case 6: return "TCP";
+                case 17: return "UDP";
+                default: return "#" + b.ToString();
+            }
+
+        }
+
+        public void Sniff(IPAddress ip)
+        {
+
+            try
+            {
+                Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Raw, ProtocolType.IP);
+                sck.Bind(new IPEndPoint(ip, 0));
+                sck.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.HeaderIncluded, true);
+                sck.IOControl(IOControlCode.ReceiveAll, new byte[4] { 1, 0, 0, 0 }, null);
+                byte[] buffer = new byte[24];
+                Action<IAsyncResult> OnReceive = null;
+                OnReceive = (ar) =>
+                {
+
+                    while (timer.ElapsedMilliseconds % 500 != 0) { }
+
+                    String[] row = { new IPAddress(BitConverter.ToUInt32(buffer, 12)).ToString(),
+                        ((ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 20))).ToString(),
+                        new IPAddress(BitConverter.ToUInt32(buffer, 16)).ToString(),
+                        ((ushort)IPAddress.NetworkToHostOrder(BitConverter.ToInt16(buffer, 22))).ToString() };
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        listView1.Items.Add(ToProtocolString(buffer.Skip(9).First())).SubItems.AddRange(row);
+                        listView1.Items[listView1.Items.Count - 1].EnsureVisible();
+                        listView1.Items[listView1.Items.Count - 1].Tag = buffer;
+                        label1.Text = "Packets Sniffed: " + listView1.Items.Count;
+                    }));
+                    buffer = new byte[24];
+                    if (running) sck.BeginReceive(buffer, 0, 24, SocketFlags.None,
+                        new AsyncCallback(OnReceive), null);
+
+                };
+                sck.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None,
+                  new AsyncCallback(OnReceive), null);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            int width = listView1.Width / listView1.Columns.Count;
+            foreach (ColumnHeader header in listView1.Columns)
+            {
+                header.Width = width - 5;
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach (ListViewItem aRandomName in listView1.SelectedItems)
+            {
+                String destAddr = aRandomName.SubItems[3].Text;
+                byte[] bData = aRandomName.Tag as byte[];
+            }
+        }
+    }
+    public class mInterface
+    {
+        public NetworkInterface iFace;
+        public String iAddr;
+
+        public mInterface(String iAddr, NetworkInterface iFace)
+        {
+            this.iFace = iFace;
+            this.iAddr = iAddr;
+        }
+        public override string ToString()
+        {
+            return iFace.Name;
+        }
+    }
+}
